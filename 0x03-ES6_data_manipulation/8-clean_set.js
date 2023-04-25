ec1a07cc8a5c62027a51cd5bd892151c57932138
codeme254 <otwomadennis365@gmail.com>
2023-04-25 14:31:13 +0300

    clean set interview question 2

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,24 +1,40 @@
 /* eslint-disable consistent-return */
-const getTheRestOfTheString = (startString, mainString) => {
-  if (!startString || startString === '') return;
-  if (typeof startString !== 'string') return;
-  if (startString.length > mainString.length) return;
-  let i = 0;
-  let finalString = '';
+// const getTheRestOfTheString = (startString, mainString) => {
+//   if (!startString || startString === '') return;
+//   if (typeof startString !== 'string') return;
+//   if (startString.length > mainString.length) return;
+//   let i = 0;
+//   let finalString = '';
 
-  while (i < startString.length) {
-    if (startString[i] !== mainString[i]) return;
-    i += 1;
-  }
+//   while (i < startString.length) {
+//     if (startString[i] !== mainString[i]) return;
+//     i += 1;
+//   }
 
-  while (i < mainString.length) {
-    finalString += mainString[i];
-    i += 1;
-  }
-  return finalString;
-};
-// getTheRestOfTheString("don", "donkey");
-// getTheRestOfTheString("bon", "bonjovi")
+//   while (i < mainString.length) {
+//     finalString += mainString[i];
+//     i += 1;
+//   }
+//   return finalString;
+// };
+// // getTheRestOfTheString("don", "donkey");
+// // getTheRestOfTheString("bon", "bonjovi")
+
+// export default function cleanSet(set, startString) {
+//   if (
+//     !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+//   ) {
+//     return '';
+//   }
+//   const finalStringsArray = [];
+//   set.forEach((element) => {
+//     const restOfStringFromCurrentElement = getTheRestOfTheString(startString, element);
+//     if (restOfStringFromCurrentElement && restOfStringFromCurrentElement !== '') {
+//       finalStringsArray.push(restOfStringFromCurrentElement);
+//     }
+//   });
+//   return finalStringsArray.join('-');
+// }
 
 export default function cleanSet(set, startString) {
   if (
@@ -26,12 +42,17 @@ export default function cleanSet(set, startString) {
   ) {
     return '';
   }
-  const finalStringsArray = [];
-  set.forEach((element) => {
-    const restOfStringFromCurrentElement = getTheRestOfTheString(startString, element);
-    if (restOfStringFromCurrentElement && restOfStringFromCurrentElement !== '') {
-      finalStringsArray.push(restOfStringFromCurrentElement);
+
+  const parts = [];
+
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
     }
-  });
-  return finalStringsArray.join('-');
+  }
+  return parts.join('-');
 }
