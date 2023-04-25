32a89904974c0c2e79c21744ea0f3ea196bd1f81
codeme254 <otwomadennis365@gmail.com>
2023-04-25 14:27:46 +0300

    clean set interview question correction 1

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,7 @@
 /* eslint-disable consistent-return */
 const getTheRestOfTheString = (startString, mainString) => {
   if (!startString || startString === '') return;
+  if (typeof startString !== 'string') return;
   if (startString.length > mainString.length) return;
   let i = 0;
   let finalString = '';
@@ -20,6 +21,11 @@ const getTheRestOfTheString = (startString, mainString) => {
 // getTheRestOfTheString("bon", "bonjovi")
 
 export default function cleanSet(set, startString) {
+  if (
+    !set && !startString && !(set instanceof Set) && typeof startString !== 'string'
+  ) {
+    return '';
+  }
   const finalStringsArray = [];
   set.forEach((element) => {
     const restOfStringFromCurrentElement = getTheRestOfTheString(startString, element);
