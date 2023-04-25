11d40cad6375d31300dc9baf453d3357b25dcbba
codeme254 <otwomadennis365@gmail.com>
2023-04-25 14:37:48 +0300

    create a map with some values

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,14 @@
+export default function groceriesList() {
+//     Apples, 10
+// Tomatoes, 10
+// Pasta, 1
+// Rice, 1
+// Banana, 5
+  const map = new Map();
+  map.set('Apples', 10);
+  map.set('Tomatoes', 10);
+  map.set('Pasta', 1);
+  map.set('Rice', 1);
+  map.set('Banana', 5);
+  return map;
+}
