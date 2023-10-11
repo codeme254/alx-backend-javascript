0f235199b6a1aaeb6b159141ce84586b8cf8999b
codeme254 <otwomadennis365@gmail.com>
2023-10-11 13:58:54 +0300

    stubs correction

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,26 +1,20 @@
-const sinon = require("sinon");
-const Utils = require("./utils");
-const { expect } = require("chai");
-const sendPaymentRequestToApi = require("./4-payment");
+const sinon = require('sinon');
+const Utils = require('./utils'); 
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./4-payment');
 
-// By using sinon.spy, make sure the math used for
-// sendPaymentRequestToApi(100, 20)
-// is the same as Utils.calculateNumber('SUM', 100, 20)
-// (validate the usage of the Utils function)
-describe("sendPaymentRequestToApi", () => {
-  // validate the usage of the Utils function
-  const spy = sinon.spy(console);
-  const stub = sinon.stub(Utils, "calculateNumber");
+describe('sendPaymentRequestToApi', () => {
+  it('sendPaymentRequestToApi calls console.log with the right arguments', () => {
+    const bigBrother = sinon.spy(console);
+    const dummy = sinon.stub(Utils, 'calculateNumber');
 
-  stub.returns(10);
-
-  sendPaymentRequestToApi(100, 20);
-  expect(stub.calledWith("SUM", 100, 20)).to.be.true;
-  expect(stub.callCount).to.be.equal(1);
-
-  expect(spy.log.calledWith("The total is: 10")).to.be.true;
-  expect(spy.log.callCount).to.be.equal(1);
-
-  spy.log.restore();
-  stub.restore();
+    dummy.returns(10);
+    sendPaymentRequestToApi(100, 20);
+    expect(dummy.calledWith('SUM', 100, 20)).to.be.true;
+    expect(dummy.callCount).to.be.equal(1);
+    expect(bigBrother.log.calledWith('The total is: 10')).to.be.true;
+    expect(bigBrother.log.callCount).to.be.equal(1);
+    dummy.restore();
+    bigBrother.log.restore();
+  });
 });
